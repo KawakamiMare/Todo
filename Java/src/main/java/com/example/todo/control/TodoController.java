81f546af157cae9d562953991e5ba6b1ea08f1cc
Mare <kawakami7799@gmail.com>
2026-02-03 22:15:40 +0900

    不要な@ResponsBodyを削除

--- Java/src/main/java/com/example/todo/control/TodoController.java
+++ Java/src/main/java/com/example/todo/control/TodoController.java
@@ -1,16 +1,13 @@
 package com.example.todo.control;
 
 import java.util.List;
-
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.todo.model.Todo;
 import com.example.todo.service.TodoService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController // @RestController = @Controller + @ResponseBody(戻り値をそのままHTTPレスポンスとして返すやつ)
+// ResponseBodyは、returnがHTTPレスポンス(JSON になる
 @RequestMapping("/api/todo")
 @CrossOrigin(origins = "https://localhost:3000") // Reactポート3000からこのバックエンドのアクセスする許可
 public class TodoController {
@@ -21,32 +18,37 @@ public class TodoController {
     }
 
     @GetMapping
-    @ResponseBody   
     public List<Todo> getTodoAll() {
         return todoService.findAll();
     }
 
     @PostMapping
-    @ResponseBody
     public Todo create(@RequestBody Todo todo) {
         return todoService.create(todo);
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
     public Todo getTodo(@PathVariable Long id) {
         return todoService.findById(id);
     }
 
     @PutMapping("/{id}")
-    @ResponseBody
-    public Todo update(@PathVariable Long id, @RequestBody Todo todo) {
+    public Todo update(@PathVariable Long id, @RequestBody Todo todo) { // RequestBodyはフロントからデータを受け取る、だから引数に入れる
         return todoService.update(id, todo);
     }
 
+    // DeleteMappingの()内のIdはフロント（todoServise.ts）がこのメソッドに到達できるようにするための看板のようなもの
     @DeleteMapping("/{id}")
-    @ResponseBody
-    public void delete(@PathVariable Long id) {
+    public void delete(@PathVariable Long id) { // 引数のidは、実際にこのメソッドがどのidを持つレコードを消すかを判断するためのもの
         todoService.delete(id);
     }
+
+//     @DeleteMapping("/{id}") // URL側は "id" のまま
+// public void delete(@PathVariable Long todoId) { 
+//     todoService.delete(todoId);
+// }
+// ↑このような、mappingと引数でid名が違う場合、
+// public void delete(@PathVariable("id") Long todoId) { ... }
+// または @PathVariable(name = "id")　　　のように書く。リファクタなどであえてidを変えたかったんだなとわかる
+
 }
